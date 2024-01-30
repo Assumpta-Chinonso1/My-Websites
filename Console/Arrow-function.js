4a592c6e89168a7a83576c8af90fb67d1823fff6
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-01-30 15:55:40 +0100

    Learnt about Arrow function syntax

---
+++ Console/Arrow-function.js
@@ -0,0 +1,30 @@
+
+  //Regular Arrow function syntax
+const add  = (a, b) => {
+    return a + b
+}
+
+//implicit return = because we are specifying the return keyword
+const subtract = (a, b) => a - b;
+
+const doube = a => a * 3 // single para? no need for parenthesis
+
+//Returning an object
+const  creatObj = () => ({
+    name: 'Assumpta'
+});
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9]
+
+numbers.forEach(function (n) {
+    console.log(n);
+    
+});
+
+//Arrow function in a callback
+numbers.forEach ((n) => console.log(n));
+
+console.log(add(1, 2));
+console.log(subtract (4, 2) );
+console.log(doube(20));
+console.log(creatObj());

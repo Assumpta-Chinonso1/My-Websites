6d37213f904e1cabda5815b41b115fbd47c19aec
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-02-17 07:59:37 +0100

    learned about Logical Operator

---
+++ Console/logical-assignment.js
@@ -0,0 +1,36 @@
+  // ||=
+let x = 20
+
+if (!x) {
+ //  x = 10 
+}
+  
+//x = x || 10
+
+x ||= 10
+
+
+console.log(x);
+
+//&&=
+
+let y = 30
+
+if (y) {
+    y = 60
+}
+  y = y && 20
+  y &&= 50
+
+console.log(y);
+
+//??=
+
+let z = null
+if (z === null || z === undefined) {
+    z = 90
+}
+ z = z ?? 90
+ z ??= 90
+
+console.log(z);

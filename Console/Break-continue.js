9a5cea75a56fa269d1e07739a2567cf16f7c8065
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-02-19 14:47:01 +0100

    learned about Break and Continue

---
+++ Console/Break-continue.js
@@ -0,0 +1,24 @@
+    //Break
+
+    for (let i = 0; i <= 20; i++) {
+        if (i === 15) {
+            console.log('Breaking...');
+            break;
+            
+        }
+        console.log(i);
+    
+}
+
+       //continue
+
+       for (let j = 5; j <= 12; j++) {
+
+         if (j === 10) {
+            console.log('Skipping 10...');
+            continue;
+         }
+
+        console.log(j);
+        
+       }

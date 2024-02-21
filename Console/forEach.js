bd3518c255b189aa400e926bd43622b1c3ce2d50
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-02-21 17:09:53 +0100

    learned about Array.forEach today

---
+++ Console/forEach.js
@@ -0,0 +1,28 @@
+const socialMedias = ['Instagram', 'Twitter', 'Facebook', 'Linkdlin']
+
+       //console.log(socialMedias._proto_);
+  //socialMedias.forEach (function (item) {
+
+    //console.log(item);
+    
+    //})
+
+    socialMedias.forEach((item, index, arr) => console.log(`${index}- ${item}`,arr));
+
+    function enterSocial(socialMedias) {
+        console.log(socialMedias);
+        
+    }
+    socialMedias.forEach(enterSocial)
+
+    const socialObj = [
+        {name:'instagram', url:  'htttp://instagram.com'},  
+        {name:'Twitter', url:  'htttp://twitter.com'}, 
+         {name:'Facebook', url:  'htttp://Facebook.com'},  
+         {name:'Linkdlin', url:  'htttp://linkdlin.com'},
+
+
+
+    ]
+
+    socialObj.forEach((item) => console.log(item.url));

22efd0de1937a91e47e5e9e824de47004e99e0b7
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-02-21 16:31:27 +0100

    for..in..loop and made some correction

---
+++ Console/for...in..loop.js
@@ -0,0 +1,22 @@
+const fruitName = {
+     fruit1: 'Avcado',
+    fruit2: 'Mango',
+    fruit3: 'Apple',
+     fruit4: 'orange'
+};
+
+for (const key in fruitName) {
+    console.log(key, fruitName[key]);
+    
+}
+
+const favColor = ['Blue', 'Black', 'Red', 'Pink'];
+
+ //for (const fav in favColor) {
+    for (const key in favColor) {
+        console.log(favColor[key]);
+        
+    
+    //console.log(fav);
+    
+}

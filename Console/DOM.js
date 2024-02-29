a62342ae3c9a2da7938996167f1a5a61c0e6af1d
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-02-29 17:34:51 +0100

    DOM intro

---
+++ Console/DOM.js
@@ -0,0 +1,12 @@
+//console.log(document.body.innerHTML);
+console.log(document.links [0]);
+
+  //document.body.innerHTML = '<h1>Hello world</h1>'
+  document.write('please subcribe to my youtube channel')
+
+  console.log(document.getElementById('main'));
+
+  const main = document.getElementById('main')
+  main.innerHTML = '<h1>Hello from the Techbabe</h1>'
+
+  document.querySelector('#main h1').innerText = 'Good'

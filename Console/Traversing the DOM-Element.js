590e6be79d020fda3954605c7b58273b89fe43b6
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-03-05 20:08:33 +0100

    Did learn  on DOM-Nodes

--- Console/Traversing the DOM-Element.js
+++ Console/Traversing the DOM-Element.js
@@ -35,6 +35,45 @@ output = secondItem.nextElementSibling
  secondItem.nextElementSibling.style.color = 'green'
  secondItem.previousElementSibling.style.color = 'orange'
 
+ //Traversing the DOM-nodes
+   
+ output = parent.childNodes
+ output = parent.childNodes[0]
+ output = parent.childNodes[0].textContent
+ output = parent.childNodes[0].nodeName
+ output = parent.childNodes[3].textContent
+ output = parent.childNodes[3].outerHTML
+ output = parent.childNodes[3].innerText = 'Child One One'
+ output = parent.childNodes[5].style.color = 'red'
+
+ output = parent.firstChild;
+ output = parent.lastChild;
+
+ output = parent.lastChild.textContent = 'Hello Dear!'
+
+ //Parent Nodes
+
+ const childs = document.querySelector('.child')
+
+ output = childs.parentNodes;
+ output = childs.parentElement;
+
+ output = childs.parentNode.style.backgroundColor = 'Black'
+
+ output = childs.parentNode.style.padding = '30px'
+
+ //siblings
+
+ const secondItems = document.querySelector('.child:nth-child(2)')
+
+output = secondItems
+
+output = secondItems.nextElementSibling
+
+ secondItems.nextElementSibling.style.color = 'green'
+ secondItems.previousElementSibling.style.color = 'orange'
 
 
-console.dir(output);
+
+
+console.log(output);

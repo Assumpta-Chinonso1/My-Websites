36090fe0b89703e6b0237fbaccd9bfa2d73e7d06
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-03-13 17:57:27 +0100

    Learned abou Mouse event

--- Console/Adding and Removing Styles and Clases.js
+++ Console/Adding and Removing Styles and Clases.js
@@ -1,36 +1,79 @@
-const text = document.querySelector('p')
-const itemList = document.querySelector('.item-list')
-const items = document.querySelectorAll('li')
+   //sssconst text = document.querySelector('p')
+   //const itemList = document.querySelector('.item-list')
+   //const items = document.querySelectorAll('li')
+
+     
 
 
-function run(){
+
+    //function run(){
 
     //ClassName
-    console.log(itemList.className);
-    text.className =  ' card dark'
+      //console.log(itemList.className);
+    ///text.className =  ' card dark'
 
     //classList
 
-    console.log(itemList.classList);
+    ///console.log(itemList.classList);
 
-    itemList.classList.forEach((c) => console.log(c));
+   //// itemList.classList.forEach((c) => console.log(c));
 
     //text.classList.add('dark')
    //text.classList.remove('card')
   // text.classList.toggle('hidden')
-  text.classList.replace('card', 'dark')
+    ///text.classList.replace('card', 'dark')
 
   //changle style
    // itemList.computedStyleMap.linHeight = '3'
 
-   items.forEach((item, index) => {
-    item.style.color = 'red'
+  /// items.forEach((item, index) => {
+   // item.style.color = 'red'
 
-    if (index === 2) {
-        item.style.color = 'blue'
+   // if (index === 2) {
+     //   item.style.color = 'blue'
         
-    }
-   });
-}
+   // }
+   //});
+   //}
 
-document.querySelector('button').onclick = run
+//document.querySelector('button').onclick = run
+
+
+
+
+
+
+
+
+//Mouse Event and Event Object
+     const logo = document.querySelector('img')
+     function onclick(event) {
+        console.log(event.target);
+        
+     }
+
+     logo.addEventListener('click', function (event) {
+        console.log(event);
+        
+     })
+
+const onClick = () => console.log('click event');
+const onDoubleClick = () => {
+
+  if ( document.body.style.backgroundColor !== 'purple') {
+    document.body.style.backgroundColor = 'purple'
+    document.body.style.color = 'white'
+  } else{
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = 'black'
+  }
+
+};
+const onRightClick = () => console.log('right click event');
+const onMouseDown = () => console.log('mouse down event');
+//Event Listener
+
+logo.addEventListener('click', onClick)
+logo.addEventListener('dblclick', onDoubleClick)
+logo.addEventListener('contextMenu', onRightClick)
+logo.addEventListener('mousedown', onMouseDown)

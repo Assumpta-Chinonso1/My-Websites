aad9aad8d0b8c51cd36f54c92eb1e2eeef37099e
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-03-17 14:12:09 +0100

    Learned about Keyboard Event and Keyboard prpertie

--- Console/Adding and Removing Styles and Clases.js
+++ Console/Adding and Removing Styles and Clases.js
@@ -46,34 +46,88 @@
 
 
 //Mouse Event and Event Object
-     const logo = document.querySelector('img')
-     function onclick(event) {
-        console.log(event.target);
+    // const logo = document.querySelector('img')
+    // function onclick(event) {
+      //  console.log(event.target);
         
-     }
+     //}
 
-     logo.addEventListener('click', function (event) {
-        console.log(event);
+     //logo.addEventListener('click', function (event) {
+      //  console.log(event);
         
-     })
+    //})
 
-const onClick = () => console.log('click event');
-const onDoubleClick = () => {
+    //const onClick = () => console.log('click event');
+   //const onDoubleClick = () => {
 
-  if ( document.body.style.backgroundColor !== 'purple') {
-    document.body.style.backgroundColor = 'purple'
-    document.body.style.color = 'white'
-  } else{
-    document.body.style.backgroundColor = 'white'
-    document.body.style.color = 'black'
-  }
+    // if ( document.body.style.backgroundColor !== 'purple') {
+    //document.body.style.backgroundColor = 'purple'
+    //document.body.style.color = 'white'
+     //} else{
+   // document.body.style.backgroundColor = 'white'
+   // document.body.style.color = 'black'
+  //}
 
-};
-const onRightClick = () => console.log('right click event');
-const onMouseDown = () => console.log('mouse down event');
+   //};
+  //const onRightClick = () => console.log('right click event');
+  //const onMouseDown = () => console.log('mouse down event');
 //Event Listener
 
-logo.addEventListener('click', onClick)
-logo.addEventListener('dblclick', onDoubleClick)
-logo.addEventListener('contextMenu', onRightClick)
-logo.addEventListener('mousedown', onMouseDown)
+   //logo.addEventListener('click', onClick)
+  //logo.addEventListener('dblclick', onDoubleClick)
+ //logo.addEventListener('contextMenu', onRightClick)
+   //logo.addEventListener('mousedown', onMouseDown)
+
+
+//Keyboard Event and Keyboard Properties
+
+const itemInput = document.getElementById('item-input')
+
+     const onKeyPress = (e) => {
+     console.log('keypress');
+   };
+
+  const onKeyUp = (e)  => {
+  console.log('keyup');
+   }
+
+const onKeyDown = (e)  => {
+         //key
+       if (e.key === 'Enter') {
+        alert('You Pressed enter')
+       }
+
+         //keyCode
+            if (e.keyCode === 13) {
+              alert('You pressed enter')
+              
+            }
+
+         //Code
+
+         if (e.code === 'Digit1') {
+          console.log('You pressed 1');
+          
+         }
+         if (e.repeat) {
+          console.log('You are holding down ' + e.key);
+         }
+         console.log('Shift ' + e.shiftKey);
+         console.log('Control ' + e.ctrlKey);
+         console.log('Alt: ' + e.altKey);
+
+
+         if (e.shiftKey && e.key === 'k') {
+          console.log('You hit shift + k');
+          
+         }
+}
+
+
+//itemInput.addEventListener('keypress', onKeyPress);
+
+//itemInput.addEventListener('keyup'. onKeyUp)
+
+itemInput.addEventListener('keydown', onKeyDown)
+
+

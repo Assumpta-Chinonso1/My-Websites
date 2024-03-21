8abcb51d6ee49b8eb11e0c22d35209f23e3b5f3b
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-03-21 08:21:38 +0100

    learn a bit about page loading

---
+++ Console/pageLoading.js
@@ -0,0 +1,16 @@
+   //window.onload = function () {
+   //document.querySelector('h1').textContent = 'Hello World'
+
+    //};
+
+  //window.addEventListener (
+   // 'load',
+   // () => (document.querySelector('h1').textContent = 'Hello people')
+//);
+
+window.addEventListener (
+    'DOMContentLoaded',
+    () => (document.querySelector('h1').textContent = 'Hello TechBabe')
+    
+);
+
